737dfbdba32de27448a9acaab6bb54bbab8118cf
hnzqy <1049027838@qq.com>
2021-09-25 12:25:10 +0800

    初始环境配置作

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import {} from "antd"
+// import "./App.js"
+import "./App.less"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      撩学堂-后台管理系统 根组件
+        <button type="primary" >
+            按钮测试
+        </button>
     </div>
   );
 }
